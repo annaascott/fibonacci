b13aa5647c8d4df235df7302b9750a98ef32741b
Anna Ascott <anna.ascott@lshtm.ac.uk>
2025-06-18 17:50:23 +0100

    recursively calculate fibonacci numbers

---
+++ Fibonacci.R
@@ -0,0 +1,21 @@
+# recursively calculate fibonacci numbers
+fibonacci_recursive<-function(n){if(n==1){return(0)} else if (n==2){return(1)} else if (n>=3){return(fibonacci_recursive(n-1)+fibonacci_recursive(n-2))}}
+
+#lets test our function
+assert_that(fibonacci_recursive(1)==0)
+
+# Recursively calculate Fibonacci numbers
+fibonacci_recursive <- function(n) {
+  if (n == 1) {
+    return(0)
+  } else if (n == 2) {
+    return(1)
+  } else if (n >= 3) {
+    return(fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2))
+  }
+}
+
+# Let's test our function
+library(assertthat)
+assert_that(fibonacci_recursive(1) == 0)
+
